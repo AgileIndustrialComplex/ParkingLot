8aae74b69c2a9fcd0e99b67dbc2d8b77854c1ebd
Adam Jedrzejewski <git@kudini.jade.waw.pl>
2025-04-22 13:13:50 +0200

    Update IParkingSpotSelector.cs

--- ParkingLot.Interfaces/IParkingSpotSelector.cs
+++ ParkingLot.Interfaces/IParkingSpotSelector.cs
@@ -1,6 +1,8 @@
 namespace ParkingLot.Interfaces;
 
+// IParkingSpotSelector selects a spot given vehicle can park on
 public interface IParkingSpotSelector
 {
+    // GetSpot selects a spot given vehicle can park on
     IParkingSpot GetSpot(IVehicle vehicle, IEnumerable<IParkingSpot> spots);
 }
